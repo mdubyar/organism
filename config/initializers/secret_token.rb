2b0d1bbb9dd3b7b404829fb5c90a0dfb4836608c
Matthew Whiting <matt@interactingorganisms.net>
2011-04-13 00:31:10 +1200

    New project add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Organism::Application.config.secret_token = 'b115c7567125e33923f922f7737fd61456b6cad18e00019e9d0be9b97b7113afa84ea1686770209d172b7c48f68833083b3fbb17a7f9b1332c9b2e008f3cb657'
